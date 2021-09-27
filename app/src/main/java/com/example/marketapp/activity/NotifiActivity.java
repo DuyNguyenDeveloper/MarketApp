074e40a01271726b60be8b900d82d12c01ee668f
DuyNguyenDeveloper <77428237+duynguyendeveloper@users.noreply.github.com>
2021-09-27 13:40:52 +0700

    hieu update

---
+++ app/src/main/java/com/example/marketapp/activity/NotifiActivity.java
@@ -0,0 +1,16 @@
+package com.example.marketapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marketapp.R;
+
+public class NotifiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifi);
+    }
+}
