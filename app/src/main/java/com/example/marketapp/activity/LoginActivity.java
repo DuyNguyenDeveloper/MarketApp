124b60bb00bafcc0da98ed65f68c53fa70cc28c3
Quốc <quocnt110698@gmail.com>
2021-11-06 21:41:04 +0700

    remember password

--- app/src/main/java/com/example/marketapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/marketapp/activity/LoginActivity.java
@@ -50,6 +50,7 @@ public class LoginActivity extends AppCompatActivity {
 
         remember = findViewById(R.id.rememberMe);
 
+
         remember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
